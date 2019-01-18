d08509592bf28af647c9bb0894fac3fdb78f5750
Saint-Jean Germila <germila@noukod.com>
2019-01-18 18:35:09 +0000

    Automatically backed up by Learn

--- db/migrate/20190118183155_create_dogs.rb
+++ db/migrate/20190118183155_create_dogs.rb
@@ -1,9 +1,12 @@
 class CreateDogs < ActiveRecord::Migration
+  def change
+
   def up
     create_table :dogs do |t|
       t.string :name
       t.string :breed
     end
+      end
   end
 
   def down
